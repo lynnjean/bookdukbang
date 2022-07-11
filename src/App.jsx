01022af8f90e9a751279908ddda864af156b7096
alswlbb <balswl0496@naver.com>
2022-07-11 11:19:29 +0900

    #16 Feat.로그인 UI 수정4

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 import LoginPage from './pages/LoginPage';
 
-const Wrapper = styled.div`
-	height: 100%;
-	width: 100%;
-	background-color: #f5f5f5;
-`;
+// const Wrapper = styled.div`
+// 	height: 100%;
+// 	width: 100%;
+// 	background-color: #f5f5f5;
+// `;
 
 function App() {
-	return (
-		<Wrapper>
-			<LoginPage />
-		</Wrapper>
-	);
+	return <LoginPage />;
 }
 
 export default App;
